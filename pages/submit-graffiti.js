868f0e5537b9aabff8d37290f42925a2e657fc21
Matheus Gama <matheus_gama@live.com>
2021-10-30 22:56:10 -0300

    feat: submit graffiti route protection

--- pages/submit-graffiti.js
+++ pages/submit-graffiti.js
@@ -1,7 +1,15 @@
+import { useRouter, useContext } from 'react'
 import SubmitGraffitiForm from '../components/graffiti/SubmitGraffitiForm'
+import AuthContext from '../store/AuthContext.js'
 
 export default function SubmitGraffiti() {
+  const router = useRouter()
+  const authCtx = useContext(AuthContext)
+
   const submitHandler = (graffitiData) => {}
+
+  if(!authCtx.isLoggedIn)
+    router.replace('/')
 
   return <SubmitGraffitiForm onSubmit={submitHandler} />
 }
